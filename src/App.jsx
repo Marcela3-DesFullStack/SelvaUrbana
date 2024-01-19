6232248dde942fff3b65a3c3de27de85f2f2a7c0
Marcela3-DesFullStack <claudiamarcela.nc14@gmail.com>
2024-01-19 19:58:18 +0100

    Including header and images folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage"
 import Header from "./pages/Header"
-=======
-import HomePage from "./pages/HomePage";
-
-const App = () => {
->>>>>>> 27dd938dacf49a4f21b0cfa2d0f1954ed315532b
 
 const App = () => {
   return (
     
     <>
-<<<<<<< HEAD
-    <Header/>
+      <Header/>
     
-    <HomePage/>
+      <HomePage/>
     
-=======
-    <HomePage />
-        
->>>>>>> 27dd938dacf49a4f21b0cfa2d0f1954ed315532b
     </>
         
   )
---
+++ src/pages/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="\src\img\logo_selva_urbana.jpg" alt="Logo Selva Urbana"/>
+            <h1> Selva Urbana</h1>
+        </div>
+    )
+}
+export default Header;
