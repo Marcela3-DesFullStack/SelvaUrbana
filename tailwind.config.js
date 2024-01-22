3e758e0084021799ac219e5c9e89bdf98a654754
Alfredo Escamilla <148432892+Alfredo-Escamilla@users.noreply.github.com>
2024-01-22 15:13:00 +0100

    -tailwind ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*/.{html,js}"],
-  [9:34]
-    theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [],
 }
-
