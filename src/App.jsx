f2714ff6ef5b507d11c4e160a6f9d4e71e453193
Aliciared7 <aliciared7@gmail.com>
2024-01-22 15:26:10 +0100

    add new products

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, { useState } from "react";
+import Footer from "./pages/Footer";
+import "./components/Footer.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>SELVA URBANA</h1>
-      <h2>Tu tienda online de planta que te lleva la naturaleza a casa</h2>
-  
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
