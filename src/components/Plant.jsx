fe24a1a9234980218f53c08976aa3a556b3e305a
Paolo Damen <paolodamen@gmail.com>
2024-01-23 17:29:28 +0100

    flexbox solved

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -21,16 +21,20 @@ const Plant = ({plants}) => {
                 <div className="card" key={plant.id}>
                     <h3 className='cardName'>{plant.name}</h3>
                     <Link to={plant.id}>                   
-                    <img src={plant.photo} alt="" className='plantImg'/> 
-                    <button className='deleteBtn'>view</button>
+                    <img src={plant.photo} alt="" className='plantImg'/>
                     </Link>
+                    <div className="buttonContainer">
+                        <Link to={plant.id}>
+                        <button className='actionBtn'>view</button>
+                        </Link>
+                        <button className='actionBtn'>edit</button>
+                        <button className='actionBtn'>delete</button>
+                    </div>
 
                     <img src={plant.type} alt="" className='interiorPlant'/>
                     <p className="stock">{plant.stock} <br />stock</p>
                     <p className="price">{plant.price} €</p>
-
-                   s
-                    
+             
                 </div>
             ))}
         </div>
