950f89ad484b68065d84ef00bf459d4bc826e499
Alfredo-Escamilla <alfredo.escamilla.gonzalez@gmail.com>
2024-01-23 06:47:32 +0100

    adjust card component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ import DeleteButton from './DeleteButton';
 
 export default function CardProduct() {
     return (
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img className="rounded-t-lg" src= "../src/assets/image-1.jpg" alt="imagen" />
+        <img className="rounded-t-sm" src= "../src/assets/image-1.jpg" alt="imagen" />
     </a>
-    <div className="p-5">
+    <div className="p-0">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <h5 className="mb-2 text-2l font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         
