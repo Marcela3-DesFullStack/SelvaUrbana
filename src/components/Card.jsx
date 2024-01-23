365d5421f7827d3812c29328f8e0d8fa07c10639
Alfredo-Escamilla <alfredo.escamilla.gonzalez@gmail.com>
2024-01-23 21:20:34 +0100

    -

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,18 +19,22 @@ export default function CardProduct() {
     return (
 <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img className="rounded-t-sm" src={products.length > 0 && products[0].photo} alt="imagen" />
+        <img className="rounded-t-sm w-64 h-64" src={products.length > 0 && products[0].photo} alt="imagen" />
     </a>
     <div className="p-0">
         <a href="#">
             <h5 className="mb-2 text-2l font-bold tracking-tight text-gray-900 dark:text-white">
-                {products.length > 0 && products[0].name}
+                Artículo: {products.length > 0 && products[0].name}
             </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {products.length > 0 && products[0].type}
+            Planta de {products.length > 0 && products[0].type}
         </p>
-        
+
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Stock: {products.length > 0 && products[0].stock} uds.  Precio: {products.length > 0 && products[0].price}€
+        </p>
+
         <div className="flex gap-2 items-center justify-between">
             <ModalWindow />
             <EditButton />
