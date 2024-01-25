56f31199fc7d1253c2c95a7ccc42109e55e00714
Paolo Damen <148433895+paolo-mad@users.noreply.github.com>
2024-01-25 20:23:17 +0100

    Update EditPage.jsx

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,108 +1,116 @@
-import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import ImageUploader from "../components/ImageUploader";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Create = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState(0);
-  const [stock, setStock] = useState(0);
-  const [description, setDescription] = useState("");
-  const [type, setType] = useState("/icons/indoor_icon.svg");
-  const [photo, setPhoto] = useState();
-  const [isPending, setIsPending] = useState(false);
-  const navigate = useNavigate();
-  const { id } = useParams(); 
 
-  useEffect(() => {
-    if (id) {
-      fetch(`http://localhost:3001/products/${id}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setName(data.name);
-          setPrice(data.price);
-          setStock(data.stock);
-          setDescription(data.description);
-          setType(data.type);
-        });
+
+const Update = () =>  {
+    const [data, setData] = useState({
+        name: "",
+        stock: "",
+        price: "",
+        description: "",
+        type: "/icons/indoor_icon.svg",
+        photo: ""
+
+    });
+    const {id} = useParams();
+    const url = `http://localhost:3001/products/${id}`
+
+    useEffect(() => {
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  }, [id]);
+    return response.json();
+  })
+  .then(data => {
+    setData(data)
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+},[url])
 
-  const handleSubmit = (e) => {
+const navigate = useNavigate()
+
+const handleSubmit = (e) => {
     e.preventDefault();
-    const plant = { name, price, stock, description, type };
+    fetch(url,{  
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+        .then (() => {
+            console.log('plant edited ');
+            navigate('/')
+        })
+}
 
-    setIsPending(true);
+const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-    // const apiUrl = id
-    //   ? `http://localhost:3001/products/${id}` 
-    //   : "http://localhost:3001/products"; 
+    reader.onload = () => {
+      setData({ ...data, photo: reader.result });
+    };
 
-    // const method = id ? "PUT" : "POST";
-
-    fetch(Url, {
-      method: PUT,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(plant),
-    })
-      .then(() => {
-        console.log(id ? "Plant updated" : "New plant added");
-        setIsPending(false);
-        navigate("/");
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setIsPending(false);
-      });
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   };
+    
+    return (
+        <div className="create">  
+        <form onSubmit={handleSubmit }>
+            <label>Nombre</label>
+            <input type="text " 
+                required
+                value={data.name}
+                onChange={(e) => setData({...data, name: e.target.value})}
+            />
+            <label>Imagen</label>
+            <input type="file"
+                
+                onChange={handleImageChange} />
+                {data.photo && <img src={data.photo} alt="Planta Seleccionada" style={{width:'200px'}}/>}
 
-  return (
-    <div className="create">
-      <h2>{id ? "Edit" : "Add"} a plant</h2>
-      <form onSubmit={handleSubmit}>
-      <label>plant name</label>
-                <input type="text " 
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <div className="photoContainer">
-                     <h1>Image Uploader</h1>
-                        <ImageUploader />
-                </div>
-
-                <label>price</label>
-                <input type="text" 
-                    required
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
-                <label>quantity</label>
-                <input type="text " 
-                    required
-                    value={stock}
-                    onChange={(e) => setStock(e.target.value)}
-                />
-                <label>description</label>
-                <textarea 
-                    required
-                    value={description}
-                    onChange={(e)=> setDescription(e.target.value)}
-                >              
-                </textarea> 
-                <label>indoor/outdoor</label>
-                <select
-                    value={type}
-                    onChange={(e) => setType(e.target.value)}
-                >
-                    <option value="/icons/indoor_icon.svg">indoor</option>
-                    <option value="/icons/outdoor_icon.svg">outdoor</option>
-                </select>
-
-        {!isPending && <button>{id ? "Update" : "Add"} plant</button>}
-        {isPending && <button disabled>{id ? "Updating" : "Adding"} plant...</button>}
-      </form>
+            <label>Precio</label>
+            <input type="text" 
+                required
+                value={data.price}
+                onChange={(e) => setData({...data, price: e.target.value})}
+            />
+            <label>Stock</label>
+            <input type="text " 
+                required
+                value={data.stock}
+                onChange={(e) => setData({...data, stock: e.target.value})}
+            />
+            <label>Descripción</label>
+            <textarea 
+                required
+                value={data.description}
+                onChange={(e) => setData({...data, description: e.target.value})}
+            >              
+            </textarea> 
+            <label>interior/exterior</label>
+            <select
+                value={data.type}
+                onChange={(e) => setData({...data, type: e.target.value})}
+            >
+                <option value="/icons/indoor_icon.svg">interior</option>
+                <option value="/icons/outdoor_icon.svg">exterior</option>
+            </select>
+            <button>actualizar</button>
+        </form>
     </div>
-  );
-};
+ );
+}
 
-export default Create;
+
+export default Update
+
